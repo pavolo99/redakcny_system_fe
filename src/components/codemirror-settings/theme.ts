45ff1f3ff2eb4ba74f4f13ee9cd5a0b6ee98538b
Pavol Dlugoš <pavol.dlugos@student.tuke.sk>
2021-10-18 14:25:46 +0200

    Style editor

--- src/components/codemirror-settings/theme.ts
+++ src/components/codemirror-settings/theme.ts
@@ -9,8 +9,10 @@ export const theme = EditorView.theme({
     minHeight: "80vh",
     overflowY: "auto",
     paddingLeft: "5%",
-    paddingTop: "5%"
+    paddingTop: "5%",
+    whiteSpace: "pre-wrap",
+    wordWrap: "break-word",
+    fontFamily: "Lato, Helvetica Neue, Helvetica, sans-serif"
   },
-  ".cm-line": {paddingTop: "3px", marginBottom: "3px", fontSize: "110%"},
   ".cm-activeLine": {backgroundColor: "white"}
 }, {dark: false})
